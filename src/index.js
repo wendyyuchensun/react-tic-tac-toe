58830132f2032877cf488d33f1a7cfa8f5aa80fd
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2017-06-29 11:05:37 +0800

    Add calcWinner function

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
           squares = current.squares.slice(),
           xIsNext = this.state.xIsNext
 
-    if (squares[i]) return
+    if (squares[i] || calcWinner(squares)) return
 
     squares[i] = xIsNext? 'X':'O'
     this.setState({
@@ -94,3 +94,24 @@ ReactDOM.render(
   <Game />,
   document.querySelector('#root')
 )
+
+function calcWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null // no winner at this point
+}
